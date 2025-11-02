4238a68bb9f076afd094144a5f6c69bb5633d580
ig-ayush <igayushacharya@gmail.com>
2025-11-02 23:37:11 +0530

    Routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div className="container flex items-center flex-col">
       <h1 className="text-2xl">Hello coders</h1>
       <h2>Lets learn Next.jS</h2>
+      <Link href="/user">user page</Link>
     </div>
   );
 }

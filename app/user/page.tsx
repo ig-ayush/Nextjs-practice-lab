a3c22c8fbb12187c3ab34d91d86ce70f86fbb688
ig-ayush <igayushacharya@gmail.com>
2025-11-16 22:24:53 +0530

    Extra style added

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const NextPage = () => {
   return (
     <>
-      <div>
-        <h1>User Page</h1>
-        <h2>Redirecter to user Page</h2>
+      <div className='w-full flex flex-col justify-center items-center'>
+        <h1 className='bg-black text-white font-bold w-full text-center flex items-center justify-center h-10'>User Page</h1>
+        <h2 className='font-bold p-5 text-2xl'>Redirecter to user Page</h2>
       </div>
     </>
   )
